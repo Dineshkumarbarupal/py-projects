7297edd722eba1aa5b02acdb1538fea3e95fb5f1
dineshkumarbarupal <dineshkumar5apd@gmail.com>
2024-09-18 15:28:06 +0530

    cafe managment system price feature

--- 05_cafe_managment_system.py
+++ 05_cafe_managment_system.py
@@ -11,17 +11,17 @@ print(f"bargar = 50rs\npizza = 150rs\ncoffee = 80rs\nsandwich = 50rs\nFor cunfir
 
 def cafe_system():
     while True:
-        item = input("Enter your food wich you want to order:")
+        item = input("Enter your food which you want to order:")
         
-
+        
+        
         if item in manu:
+            price = manu[item]
             print(f"your ordered item {item} added succesfully.")     
         elif item == "ok":
-            order_total = 0
-            for price in item:
-              order_total += price
+            
             print("thank you for order.") 
-            print(f"your total price is ")   
+            print(f"your total price is {price} ")   
             break   
         elif item not in manu:
             print(f"Item {item} not in manu.")    
